1e7a2147e844edf9d59dbc5b7d4f0eb5858eb67a
tbcey74123 <tbcey74123@gmail.com>
2014-12-04 17:52:53 +0800

    UPDATE.

--- admin/delete_house.php
+++ admin/delete_house.php
@@ -4,12 +4,22 @@
 	require("../sql/mysql_connection.php");
 
 	$id = $_GET['id'];
-	
+	$dir = "../pic/houses/case-" . $id . "/";
+
 	$sql = "DELETE FROM houses WHERE id = '$id'";
 	$result = mysqli_query($con, $sql);
 	//$row = mysqli_fetch_assoc($result);
-
+	
 	if(mysqli_query($con, $sql)) {
+		if(file_exists($dir)) {
+			$objs = scandir($dir);
+
+			foreach($objs as $obj) {
+				if($obj != "." && $obj != "..")
+					unlink($dir . $obj);
+			}
+			rmdir($dir);
+		}
 		echo "成功刪除！</br>";
 		echo "<a href=\"admin_update.php\">點我返回</a>";
 	}else {
